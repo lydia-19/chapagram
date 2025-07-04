d778ab2a3874202f6b1a826bfefe7e3891f63e81
Lydia Solomon <lydiasolomon97@gmail.com>
2025-07-04 08:44:29 +0300

    refactor: remove phone island section

--- src/app/components/common/PhoneWrapper/index.tsx
+++ src/app/components/common/PhoneWrapper/index.tsx
@@ -8,18 +8,12 @@ interface PhoneWrapperProps {
 const PhoneWrapper = ({ children, className = "" }: PhoneWrapperProps) => {
   return (
     <div
-      className={`lg:w-phoneMd relative w-phoneSm xl:w-phoneLg ${className}`}
+      className={`relative w-phoneSm lg:w-phoneMd xl:w-phoneLg ${className}`}
     >
       {/* Phone frame */}
       <div className="relative rounded-phoneSm bg-phone-frame p-phone shadow-xl xl:rounded-phoneLg">
-        {/* Dynamic Island */}
-        <div className="absolute left-1/2 top-0 z-40 h-island w-islandSm -translate-x-1/2 rounded-b-island bg-phone-frame xl:w-islandLg">
-          {/* Camera cutout */}
-          <div className="absolute left-1/2 top-cameraTop h-camera w-camera -translate-x-cameraOffset rounded-full bg-phone-camera ring-2 ring-phone-ring" />
-        </div>
-
         {/* Screen container */}
-        <div className="lg:h-phoneMd relative h-phoneSm w-full overflow-hidden rounded-screenSm xl:h-phoneLg xl:rounded-screenLg">
+        <div className="relative h-phoneSm w-full overflow-hidden rounded-screenSm lg:h-phoneMd xl:h-phoneLg xl:rounded-screenLg">
           {/* Content */}
           {children}
         </div>
